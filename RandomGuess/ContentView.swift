58dd761148fe72920d8caf77097c3099cc5ef07f
Hari-Rait <86234153+Hari-Rait@users.noreply.github.com>
2023-10-25 23:17:07 +0200

    Erstes Layout mit Userinput und Append in Array

--- RandomGuess/ContentView.swift
+++ RandomGuess/ContentView.swift
@@ -8,10 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var Guesses: String = ""
+    @State private var RandomGuess: [String] = []
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            TextField("Gib bitte ein Guess ein", text: $Guesses)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Button( action: {
+                if !Guesses.isEmpty {
+                    RandomGuess.append(Guesses)
+                    Guesses = ""
+                }
+            }) {
+                Text("Hinzufügen")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            List(RandomGuess, id: \.self) { item in
+                Text(item)
+            }
+            
+            Spacer()
+
+            }
+        .padding()
+        }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
