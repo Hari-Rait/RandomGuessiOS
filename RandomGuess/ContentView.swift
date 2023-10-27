0c9d2ef80746c019ca5cb7e7a5b9621a79eb4650
Hari-Rait <86234153+Hari-Rait@users.noreply.github.com>
2023-10-27 15:53:24 +0200

    Now it has a Deleteall Button whitch Deletes all Elements from the Array

--- RandomGuess/ContentView.swift
+++ RandomGuess/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     
     @State private var Guesses: String = ""
     @State private var RandomGuess: [String] = []
-    @State private var random = "Drücken Sie den Button"
+    @State private var random = ""
     
     
     var body: some View {
@@ -19,22 +19,39 @@ struct ContentView: View {
         VStack{
             
             TextField("Gib bitte ein Guess ein", text: $Guesses)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .textFieldStyle(.roundedBorder)
                 .padding()
+                .submitLabel(.done)
             
-            Button( action: {
-                if !Guesses.isEmpty {
-                    RandomGuess.append(Guesses)
-                    Guesses = ""
+            HStack{
+                Button( action: {
+                    if !Guesses.isEmpty {
+                        RandomGuess.append(Guesses)
+                        Guesses = ""
+                    }
+                }) {
+                    Text("Hinzufügen")
+                        .padding()
+                        .background(Color.white)
+                        .foregroundColor(.black)
+                        .cornerRadius(10)
                 }
-            }) {
-                Text("Hinzufügen")
-                    .padding()
-                    .background(Color.mint)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+                .shadow(color: .blue, radius: 15, y: 5)
+                .padding()
+                
+                Button ( action: {
+                    if !RandomGuess.isEmpty {
+                        random = RandomGuess.randomElement()!
+                    }
+                }) {
+                    Text("Zufall generieren")
+                        .padding()
+                        .background(Color.white)
+                        .foregroundColor(.black)
+                        .cornerRadius(10)
+                }
+                .shadow(color: .red, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
             }
-            
             List{
                 ForEach(RandomGuess, id: \.self) { item in
                     Text(item)
@@ -46,23 +63,25 @@ struct ContentView: View {
             
             Button ( action: {
                 if !RandomGuess.isEmpty {
-                    random = RandomGuess.randomElement()!
+                    RandomGuess.removeAll()
                 }
             }) {
-                Text("Zufall generieren")
+                Text("Alle Eingaben Löschen")
                     .padding()
-                    .background(Color.purple)
+                    .background(Color.black)
                     .foregroundColor(.white)
                     .cornerRadius(10)
             }
+            
             Text(random)
+                .font(.largeTitle)
             
         }
         .padding()
     }
     
     func removeRows(at offsets: IndexSet) {
-       RandomGuess.remove(atOffsets: offsets)
+        RandomGuess.remove(atOffsets: offsets)
     }
 }
 
