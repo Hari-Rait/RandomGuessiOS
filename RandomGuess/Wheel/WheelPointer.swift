4aef5dc49bd8894773813412ec2a4e3aec80dfcc
Hari-Rait <86234153+Hari-Rait@users.noreply.github.com>
2023-11-02 11:12:45 +0100

    A new View with a spinning Wheel which takes the input from the User

---
+++ RandomGuess/Wheel/WheelPointer.swift
@@ -0,0 +1,25 @@
+//
+//  WheelPointer.swift
+//  RandomGuess
+//
+//  Created by Hari Rait on 30.10.23.
+//
+
+import SwiftUI
+
+@available(iOS 14.0, *)
+struct WheelPointer: Shape {
+
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: 0))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+            path.addArc(
+                center: CGPoint(x: rect.midX, y: rect.midY),
+                radius: rect.size.width / 2,
+                startAngle: .degrees(0),
+                endAngle: .degrees(180),
+                clockwise: false)
+        }
+    }
+}
---
+++ RandomGuess/Wheel/WheelSliceShape.swift
@@ -0,0 +1,18 @@
+//
+//  WheelSliceShape.swift
+//  RandomGuess
+//
+//  Created by Hari Rait on 30.10.23.
+//
+
+import SwiftUI
+
+struct WheelSliceShape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WheelSliceShape()
+}
