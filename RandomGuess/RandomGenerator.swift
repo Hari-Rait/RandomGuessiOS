b4175e9311e3d63a676e114dbc0364bfed58eca3
Hari-Rait <86234153+Hari-Rait@users.noreply.github.com>
2023-11-13 22:04:16 +0100

    A New View and some small changes

---
+++ RandomGuess/RandomGenerator.swift
@@ -0,0 +1,18 @@
+//
+//  RandomGenerator.swift
+//  RandomGuess
+//
+//  Created by Hari Rait on 08.11.23.
+//
+
+import SwiftUI
+
+struct RandomGenerator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RandomGenerator()
+}
